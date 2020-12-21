75df96ada605ca9a0f6e0a1e97e98066cddaf0c8
popkovaanna101@gmail.com <popkovaanna101@gmail.com>
2020-12-21 21:20:57 +0300

    operator overloading

--- ООП/КлассArray/array.h
+++ ООП/КлассArray/array.h
@@ -7,17 +7,30 @@ class Array
 private:
 	int* arr;
 	int size;
+
 public:
 	Array(int _size = 10);
 	Array(const Array& copyArr);
-
-	void Sortr();
-	void SetSize();
-	int GetSize();
-	void FillArr();
-	int Min();
-	int Max();
-	void Print();
 	
+	Array operator+(const Array& b);
+	Array operator-(const Array& b);
+	Array operator/(const Array& b);
+	Array operator*(const Array& b);
+
+	Array operator++();
+	Array operator++(int k);
+	int& operator[](int indx); //оператор доступа
+	void operator()(int newSize);//вместо set (для изменения значений)
+
+	friend istream& operator>>(istream&, Array&);
+	friend ostream& operator<<(ostream&, const Array&); //конст чтобы не менять значения, тк это оператор вывода
+
+	void Print();
+	//void Sortr();
+	//void SetSize();
+	//int GetSize();
+	void FillArr();
+	//int Min();
+	//int Max();
 	~Array();
 };
