013b6748583793883e0c06301b04492b8d9e22df
popkovaanna101@gmail.com <popkovaanna101@gmail.com>
2021-01-12 21:20:04 +0300

    fix

--- ООП/Queue/main.cpp
+++ ООП/Queue/main.cpp
@@ -4,7 +4,23 @@ using namespace std;
 
 int main()
 {
+	Game game(3);
+	cout << game << endl;
 
+	game.StartGame();
+
+	//QueueRing queueRing(10);
+	//cout << queueRing;
+
+	//while (!queueRing.IsFull())
+	//	queueRing.Push(rand() % 10);
+
+	//cout << queueRing;
+	//
+	//queueRing.Pop();
+	//queueRing.Pop();
+
+	//cout << queueRing;
 
 
 }
--- ООП/Queue/queue.h
+++ ООП/Queue/queue.h
@@ -2,26 +2,40 @@
 #include <iostream>
 using namespace std;
 
-class QueueRing
+class QueueRing	//кольцевая очередь
 {
 private:
-	int* Wait;  //очередь
+	int* wait;  //очередь
 	int MaxQueueLength;  // максимальный размер очереди
-	int QueueLength;  //текущий размер очереди
-	int MaxQueueLength;  // максимальный размер очереди
+	int QueueLength = 0;  //текущий размер очереди
 
 public:
-	QueueRing(int m);
+	QueueRing(int size = 5);
 	~QueueRing();
 
-	void Push(int c); //добавление
+	void Push(int value); //добавление
 	bool Pop();	//извлечение
 	void Clear();
 	bool IsEmpty();
 	bool IsFull();
 	int GetCount(); //количество элементов
-	void Show();
+	
+	friend ostream& operator <<(ostream&, const QueueRing&);
 
+};
 
+class Game
+{
+private:
+	int countWaits = 3;
+	QueueRing* waits;
 
+	void FillWaits();
+public:
+	Game(int);
+	~Game();
+
+	void StartGame();
+
+	friend ostream& operator <<(ostream&, const Game&);
 };
