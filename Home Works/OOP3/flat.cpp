c6b92c4f621f8a22b8b45dbcf80e018373456795
annaovsy <popkovaanna101@gmail.com>
2021-01-12 15:15:24 +0300

    hw operator

---
+++ Home Works/OOP3/flat.cpp
@@ -0,0 +1,50 @@
+#include "flat.h"
+#include <iostream>
+using namespace std;
+
+Overcoat::Overcoat(const char* _type, int _coast) : coast(_coast)
+{
+	size = strlen(_type) + 1;
+	type = new char[size];
+	strcpy_s(type, size, _type);
+}
+
+void Overcoat::SetType(const char* _type)
+{
+	if (_type == NULL)
+		return;
+
+	strcpy_s(type, size, _type);
+}
+
+//Overcoat& Overcoat::operator>(Overcoat& ob2)
+//{
+//	if()
+//	coast = ob2.coast;
+//}
+
+Overcoat& Overcoat::operator=(Overcoat& ob2)
+{
+	if (type != nullptr)
+	{
+		delete[]type;
+	}
+	type = new char[strlen(type) + 1];
+	strcpy_s(type, strlen(type) + 1, type);
+
+	type = ob2.type;
+	coast= ob2.coast;
+	return *this;
+}
+
+void Overcoat::Print()
+{
+	cout << "Тип : " << type << endl;
+	cout << "Стоимость : " << coast << endl;
+}
+
+Overcoat::~Overcoat()
+{
+	if (type)
+		delete[]type;
+}
