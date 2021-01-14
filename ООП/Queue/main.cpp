a6968019a1a84d954d9612df7f68829a0ebf92dd
popkovaanna101@gmail.com <popkovaanna101@gmail.com>
2021-01-14 21:21:26 +0300

    fix

--- ООП/Queue/main.cpp
+++ ООП/Queue/main.cpp
@@ -1,13 +1,49 @@
 #include "queue.h"
+#include "QueuePr.h"
+#include "deque.h"
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	Game game(3);
-	cout << game << endl;
+	Deque deque;
+	cout << deque;
+	/*while (!deque.IsFull())
+	{
+		deque.PushBack(rand() % 10);
+	}*/
+	while (!deque.IsFull())
+	{
+		deque.PushFront(rand() % 10);
+	}
+	cout << deque;
+	deque.PopBack();
+	cout << deque;
+	cout << "Back : " << deque.Back() << endl;
+	cout << "Size : "<<deque.Size() << endl;
 
-	game.StartGame();
+
+	//QueuePriority queue;
+	//
+	//while (!queue.IsFull())
+	//{
+	//	queue.Push(rand() % 10, rand() % 3);
+	//}
+
+	//cout << queue;
+	//cout << "------------" << endl;
+
+	//int c = 0;
+	//int p = 0;
+	//while (queue.Pop(c, p))
+	//{
+	//	cout << c << " [ " << p << " ] " << endl;
+	//}
+
+	//Game game(3);
+	//cout << game << endl;
+
+	//game.StartGame();
 
 	//QueueRing queueRing(10);
 	//cout << queueRing;
