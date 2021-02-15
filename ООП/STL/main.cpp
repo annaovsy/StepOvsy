306a9757e11d2d1420540c7120eb0119e2a601d7
Anna Ovsyannikova <popkovaanna101@gmail.com>
2021-02-15 19:56:22 +0300

    fix

--- ООП/STL/main.cpp
+++ ООП/STL/main.cpp
@@ -60,54 +60,54 @@ void Change(vector<int> &vect) //если передаем не по ссылке, то ничего не помен€
 		vect[i] = 555;
 }
 
-int main()
-{
-	////vector - массив
-	//vector<int> vect;  //объ€вили вектор дл€ целых чисел
-	//vector<double> dVect(5);  //вектор на 5 элементов
-	//vector<char> cVect(5, 'x');
-
-	//cout << "vect.size() = " << vect.size() << endl;
-	//cout <<"dVect.size() = "<< dVect.size() << endl;
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	vect.push_back(i + 1);  //добавление элемента
-	//}
-	//cout << "vect.size() = " << vect.size() << endl;
-	//for (int i = 0; i < vect.size(); i++)
-	//	cout<< vect[i] << " "; //выводит все элементы
-	//cout << endl;
-	//vect.resize(100); //мен€ет размер вектора
-	//cout << "resize vect.size() = " << vect.size() << endl;
-	//for (int i = 0; i < vect.size(); i++)
-	//	vect[i] = i + 1; //заполнение элементов
-	//ShowVector(vect); //100 элементов
-	//Change(vect);     //мен€ем размер на 10
-	//ShowVector(vect); //10 элементов
-	//vector<int>::iterator ptr = vect.begin();
-	//ptr += 2;
-	//vect.insert(ptr,10, 777); //вставл€ет 777 вместо 3го элемента 10 раз
-	//ShowVector(vect);
-
-	//vector<int>::iterator ptr2 = vect.begin();
-	//ptr2 += 2;
-	//vect.erase(ptr2+3, vect.end()); //удал€ет с 5 элемента до конца
-	//reverse(vect.begin(), vect.end()); //переворачивает 
-
-	//ShowVector(vect);
-	//sort(vect.begin(), vect.end()); //сортирует от начала до конца, мен€ет элементы местами
-	//ShowVector(vect);
-
-	//vector <Demo> testV;
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	Demo temp(rand());
-	//	testV.push_back(temp);
-	//}
-	//ShowDemoVect(testV);
-	//sort(testV.begin(), testV.end());
-	//ShowDemoVect(testV);
-	 
-	TestList();
-
-}
+//int main()
+//{
+//	//vector - массив
+//	vector<int> vect;  //объ€вили вектор дл€ целых чисел
+//	vector<double> dVect(5);  //вектор на 5 элементов
+//	vector<char> cVect(5, 'x');
+//
+//	cout << "vect.size() = " << vect.size() << endl;
+//	cout <<"dVect.size() = "<< dVect.size() << endl;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		vect.push_back(i + 1);  //добавление элемента
+//	}
+//	cout << "vect.size() = " << vect.size() << endl;
+//	for (int i = 0; i < vect.size(); i++)
+//		cout<< vect[i] << " "; //выводит все элементы
+//	cout << endl;
+//	vect.resize(100); //мен€ет размер вектора
+//	cout << "resize vect.size() = " << vect.size() << endl;
+//	for (int i = 0; i < vect.size(); i++)
+//		vect[i] = i + 1; //заполнение элементов
+//	ShowVector(vect); //100 элементов
+//	Change(vect);     //мен€ем размер на 10
+//	ShowVector(vect); //10 элементов
+//	vector<int>::iterator ptr = vect.begin();
+//	ptr += 2;
+//	vect.insert(ptr,10, 777); //вставл€ет 777 вместо 3го элемента 10 раз
+//	ShowVector(vect);
+//
+//	vector<int>::iterator ptr2 = vect.begin();
+//	ptr2 += 2;
+//	vect.erase(ptr2+3, vect.end()); //удал€ет с 5 элемента до конца
+//	reverse(vect.begin(), vect.end()); //переворачивает 
+//
+//	ShowVector(vect);
+//	sort(vect.begin(), vect.end()); //сортирует от начала до конца, мен€ет элементы местами
+//	ShowVector(vect);
+//
+//	vector <Demo> testV;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		Demo temp(rand());
+//		testV.push_back(temp);
+//	}
+//	ShowDemoVect(testV);
+//	sort(testV.begin(), testV.end());
+//	ShowDemoVect(testV);
+//	 
+//	//TestList();
+//
+//}
