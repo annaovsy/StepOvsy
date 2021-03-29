8cde428785524405e9fcc62ccf90e21aed783e7a
Anna Ovsyannikova <popkovaanna101@gmail.com>
2021-03-29 20:58:58 +0300

    fix

---
+++ Csharp/ConsoleApp1/Ex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Ex
+    {
+        private int val;
+
+        public int Num  //это СВОЙСТВО. вместо сеттеров и геттеров
+        {
+            get { return val; }
+
+            set 
+            {
+                if (value >= 0 && value <= 100)
+                    val = value; 
+            }
+        }
+
+       // public int Numm => val; //Свойство только для чтения (только get) 
+
+        public void SetSize(int _val)
+        {
+            val = _val;
+        }
+        public int GetVal()
+        {
+            return val;
+        }
+    }
+}
---
+++ Csharp/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = Sum(new int[] { 1, 2, 3 });
+            Console.WriteLine($"s={s}");
+            s = Sum(new int[] { 10, 20, 30, 40 });
+            Console.WriteLine($"s={s}");
+
+            Ex ex = new Ex();
+            ex.Num = 10;
+            Console.WriteLine("val = " + ex.Num);
+
+            Employee em1 = new Employee
+            {
+                Name = "Иванов",
+                Age = 25,
+                Salary = 30000
+            };
+
+            Employee em2 = new Employee();
+            em2.Age = 35;
+            em2.Salary = -100;
+
+            Console.WriteLine(em1.Print());
+            Console.WriteLine(em2.Print());
+        }
+
+        private static int Sum(params int[] args)
+        {
+            int sum = 0;
+            foreach(var s in args)
+            {
+                sum += s;
+            }
+            return sum;
+        }
+    }
+}
