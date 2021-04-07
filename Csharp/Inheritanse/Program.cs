c7e3ee4a8e0fb111a571fb95cd8650621712076a
Anna Ovsyannikova <popkovaanna101@gmail.com>
2021-04-07 21:18:27 +0300

    fix

--- Csharp/Inheritanse/Program.cs
+++ Csharp/Inheritanse/Program.cs
@@ -51,7 +51,39 @@ namespace Inheritanse
             base.Print(); //сначала вызовет Print из базового класса, затем из наследника
             Console.WriteLine($"\nЗарплата: {_salary}\n");
         }
-    } 
+    }
+
+    class Manager : Employee
+    {
+        private string _activity;
+
+        public Manager(string fName, string lName, DateTime date, double salary, string activity)
+            :base (fName,lName,date,salary)
+        {
+            _activity = activity;
+        }
+
+        public void ShowManager()
+        {
+            Console.WriteLine($"Менеджер. Деятельность {_activity} ");
+        }
+    }
+
+    class Scinetist:Employee
+    {
+        private string _scientificDirection;
+
+        public Scinetist(string fName, string lName, DateTime date, double salary, string scientificDirection)
+            :base(fName, lName, date, salary)
+        {
+            _scientificDirection = scientificDirection;
+        }
+
+        public void ShowScinetist()
+        {
+            Console.WriteLine($"Ученый. Научное направление {_scientificDirection} ");
+        }
+    }
 
     class Program
     {
@@ -59,6 +91,33 @@ namespace Inheritanse
         {
             Employee empl = new Employee("Иванов", "Иван", new DateTime(1996,05,23), 20000);
             empl.Print();
+
+            Employee manager = new Manager("Петров", "Петр", new DateTime(1997, 06, 23), 35000, "Продукты питания");
+
+            Employee[] empls = new Employee[]
+            {
+                manager,
+                new Scinetist("Сидоров", "Владимир", new DateTime(1997, 06, 23), 37000, "История")
+            };
+
+            foreach(Employee item in empls)
+            {
+                if (item is Manager)
+                {
+                    Manager man = item as Manager;
+                    man.ShowManager();
+                }
+                else if(item is Scinetist)
+                {
+                    Scinetist scinetist = item as Scinetist;
+                    scinetist.ShowScinetist();
+                }
+                else
+                {
+                    item.Print();
+                }
+            }
+
         }
     }
 }
