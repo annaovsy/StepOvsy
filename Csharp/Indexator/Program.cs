d2a41e5ab21aeac449ece927a10d2c65dd53a67e
Anna Ovsyannikova <popkovaanna101@gmail.com>
2021-04-07 20:33:41 +0300

    fix inheritance

---
+++ Csharp/Indexator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexator
+{
+    class Laptop
+    {
+        public string Vendor { get; set; }
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Vendor} {Price}";
+        }
+    }
+
+    class Shop
+    {
+        Laptop[] laptops;
+
+        public Shop(int size)
+        {
+            laptops = new Laptop[size];
+        }
+
+        public int Length => laptops.Length;
+        
+        public Laptop this[int indx]
+        {
+            get
+            {
+                if (indx >= 0 && indx < laptops.Length) 
+                {
+                    return laptops[indx];
+                }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                laptops[indx] = value;
+            }
+        }
+        public Laptop this[string name]
+        {
+            get
+            {
+                for(int i = 0; i < laptops.Length; i++)
+                {
+                    if (name == laptops[i].Vendor)
+                        return laptops[i];
+                }
+                throw new Exception("Laptop not found");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop myShop = new Shop(5);
+            myShop[0] = new Laptop
+            {
+                Price = 30000,
+                Vendor = "HP"
+            };
+            myShop[1] = new Laptop
+            {
+                Price = 45000,
+                Vendor = "Samsung"
+            };
+            myShop[2] = new Laptop
+            {
+                Price = 50000,
+                Vendor = "Asus"
+            };
+            for (int i = 0; i < myShop.Length; i++) 
+            {
+                Console.WriteLine(myShop[i]);
+            }
+            Console.WriteLine(myShop["Asus"]);
+        }
+    }
+}
---
+++ Csharp/Inheritanse/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritanse
+{
+    class Human
+    {
+        private int _id;
+        protected string firstName;
+        protected string lastName;
+        protected DateTime birthday; //тип данных для времени!!!
+
+        public Human(string fName, string lName) : this(fName, lName, DateTime.MinValue) //делегирование конструктора
+        {}
+
+        public Human(string fName, string lName, DateTime date)
+        {
+            firstName = fName;
+            lastName = lName;
+            birthday = date;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Фамилия: {firstName}\nИмя: {lastName}\nДата рождения: {birthday.ToShortDateString()}"); // ToShortDateString выведет дату без времени
+        }
+
+    }
+
+    class Employee:Human
+    {
+        double _salary;
+
+        public Employee(string fName, string lName) : base(fName, lName) { }
+
+        public Employee(string fName,string lName, double salary) : base(fName, lName)
+        {
+            _salary = salary;
+        }
+
+        public Employee(string fName, string lName,DateTime date, double salary) : base(fName, lName, date)
+        {
+            _salary = salary;
+        }
+
+        public new void Print() 
+        {
+            base.Print(); //сначала вызовет Print из базового класса, затем из наследника
+            Console.WriteLine($"\nЗарплата: {_salary}\n");
+        }
+    } 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee empl = new Employee("Иванов", "Иван", new DateTime(1996,05,23), 20000);
+            empl.Print();
+        }
+    }
+}
