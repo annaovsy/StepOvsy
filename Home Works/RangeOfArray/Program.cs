f0d35b380bd71624a430e98ae9b08e3f81c4c46e
annaovsy <popkovaanna101@gmail.com>
2021-04-12 16:15:09 +0300

    fix hw

---
+++ Home Works/RangeOfArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start, end;
+            Console.Write("Начало: ");
+            start = int.Parse(Console.ReadLine());
+            Console.Write("Конец: ");
+            end = int.Parse(Console.ReadLine());
+            RangeOfArray array = new RangeOfArray(start, end);
+
+            for (int i = start; i <= end; i++)
+            {
+                Console.WriteLine($"array[{i}] - {i}"); ;
+            }
+        }
+    }
+}
