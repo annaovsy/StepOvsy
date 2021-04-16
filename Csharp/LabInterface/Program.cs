72c4549cf490515c49651159b9a43f7b684c82c5
annaovsy <popkovaanna101@gmail.com>
2021-04-16 17:33:29 +0300

    fix lab Interface

--- Csharp/LabInterface/Program.cs
+++ Csharp/LabInterface/Program.cs
@@ -10,15 +10,30 @@ namespace LabInterface
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("SimpleCipher");
             SimpleCipher simpleCipher = new SimpleCipher();
-
-            string str=simpleCipher.Encode("abc");
+            string str = simpleCipher.Encode("abc");
             Console.WriteLine(str);
-
             str = simpleCipher.Decode(str);
-            Console.WriteLine(str);
+            Console.WriteLine(str + "\n");
 
+            Console.WriteLine("BitCipher");
+            BitCipher bit = new BitCipher(16);
+            string strBit = bit.Encode("abc");
+            Console.WriteLine(strBit);
+            strBit = bit.Decode(strBit);
+            Console.WriteLine(strBit + "\n");
 
+            UnlistedPhone[] unlistedPhones =
+            {
+                new UnlistedPhone("Мария", "8-952-815-96-97", new BitCipher(6)),
+                new UnlistedPhone("Борис", "8-988-213-86-06", new BitCipher(19)),
+                new UnlistedPhone("Ксения", "8-961-599-78-00", new BitCipher(23)),
+            };
+            foreach(UnlistedPhone unlistedPhone in unlistedPhones)
+            {
+                Console.WriteLine(unlistedPhone.ToString());
+            }
         }
     }
 }
