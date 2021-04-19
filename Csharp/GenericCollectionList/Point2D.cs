7a22f3434039c0c88317db64fdb77c734adfc208
Anna Ovsyannikova <popkovaanna101@gmail.com>
2021-04-19 21:09:09 +0300

    fix generic

---
+++ Csharp/GenericCollectionList/Point2D.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollectionList
+{
+    class Point2D<T> //позволяет создавать обобщенный тип данных
+    {
+        public T X { get; set; }
+        public T Y { get; set; }
+
+        public Point2D()
+        {
+            X = default;
+            Y = default; //на основе реального типа данных присвоит либо null либо 0
+
+        }
+
+    }
+}
