35665df055c2f5c69b7e88978de65c634f129ed9
Anna Ovsyannikova <popkovaanna101@gmail.com>
2021-04-22 21:15:41 +0300

    fic

---
+++ Csharp/Delegate/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public delegate double CalcDelegate(double x, double y); //делегат
+
+    class Calculator
+    {
+        public double Add(double x,double y)
+        {
+            return x + y;
+        }
+
+        public double Sub(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Mult(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Div(double x, double y)
+        {
+            if (y != 0)
+            {
+                return x / y;
+            }
+            throw new DivideByZeroException();
+        }
+
+    }
+}
