028bb2529a1206f9040f48e238e657cc5a2e0794
annaovsy <popkovaanna101@gmail.com>
2021-05-03 22:35:19 +0300

    fix DelegateEventTest2

---
+++ Csharp/DelegateEventTest2/IPropertyChanged.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEventTest2
+{
+    public class PropertyEventArgs : EventArgs
+    {
+        public string Name { get; set; }
+    }
+
+    public delegate void PropertyeventHendler(object sender, PropertyEventArgs e);
+
+    interface IPropertyChanged
+    {
+        event PropertyeventHendler PropertyChanged;
+    }
+}
