872c964c42089a05c3eedbbf8ec93f35707938e3
annaovsy <popkovaanna101@gmail.com>
2021-05-12 14:07:08 +0300

    fix exam

---
+++ Home Works/ExamQuiz/Quiz/Answer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamQuiz.Quiz
+{
+    class Answer
+    {
+        public string AnswerText { get; set; }
+        public bool CorrectAnswer { get; set; }
+     
+
+        public bool IsCorrect(bool correctAnswer)
+        {
+            if (correctAnswer)
+                return true;
+            else
+                return false;
+        }
+    }
+}
