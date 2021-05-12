a75580e104743648c1660cf3729e68bfcfaab40d
Anna Ovsyannikova <popkovaanna101@gmail.com>
2021-05-12 19:06:05 +0300

    fix

---
+++ Csharp/ExtentionsTestLINQ/ExWithoutPassword.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionsTestLINQ
+{
+    static class ExWithoutPassword
+    {
+        public static User WithoutPassword(this User user)
+        {
+            User temp = new User();
+            temp.Password = null;
+            temp.Login = user.Login;
+            temp.Role = user.Role;
+            temp.Token = user.Token;            
+
+            return temp;
+        }
+
+    }
+}
---
+++ Csharp/ExtentionsTestLINQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionsTestLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User
+            {
+                Login = "operator",
+                Password = "123",
+                Role = "admin",
+                Token = "fxg"
+            };
+
+            var user = user1.WithoutPassword();
+
+
+
+        }
+    }
+}
