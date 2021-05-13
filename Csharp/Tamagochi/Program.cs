e6923f4bb290894b796db7363ee236fa71ea84ce
Anna Ovsyannikova <popkovaanna101@gmail.com>
2021-05-13 19:15:21 +0300

    fix

---
+++ Csharp/Tamagochi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; //в проект добавили ссылку
+
+namespace Tamagochi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(MessageBox.Show("привет", "Сообщение", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                Console.WriteLine("нажали ок");
+            }
+            else
+            {
+                Console.WriteLine("нажали Cencel");
+            }
+            int n = 100;
+            //MessageBox.Show($"n = {n}");
+
+        }
+    }
+}
