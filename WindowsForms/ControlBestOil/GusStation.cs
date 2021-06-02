707275971aa264b971c69c8e8a3d4a71d5b8e195
Anna Ovsyannikova <popkovaanna101@gmail.com>
2021-06-02 20:10:18 +0300

    fix

---
+++ WindowsForms/ControlBestOil/GusStation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBestOil
+{
+    public struct Oils
+    {
+        string name;
+        double price;
+        double quantity;
+        double sum;
+    }
+
+    class GusStation
+    {
+        List<Oils> oils = new List<Oils>();
+    }
+}
