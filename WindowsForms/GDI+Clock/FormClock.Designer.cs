62b5f74615c9c318eb0d78faf5992c4f2dd2ec0f
Anna Ovsyannikova <popkovaanna101@gmail.com>
2021-06-16 20:43:53 +0300

    fix Clock

---
+++ WindowsForms/GDI+Clock/FormClock.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace GDI_Clock
+{
+    partial class FormClock
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxClock = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxClock
+            // 
+            this.pictureBoxClock.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxClock.Name = "pictureBoxClock";
+            this.pictureBoxClock.Size = new System.Drawing.Size(450, 450);
+            this.pictureBoxClock.TabIndex = 0;
+            this.pictureBoxClock.TabStop = false;
+            this.pictureBoxClock.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxClock_Paint);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // FormClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 475);
+            this.Controls.Add(this.pictureBoxClock);
+            this.Name = "FormClock";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Часы";
+            this.Load += new System.EventHandler(this.FormClock_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClock)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxClock;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ WindowsForms/GDI+Clock/FormClock.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GDI_Clock
+{
+    public partial class FormClock : Form
+    {
+        public FormClock()
+        {
+            InitializeComponent();
+        }
+
+        private void FormClock_Load(object sender, EventArgs e)
+        {
+            DoubleBuffered = true; //для предотвращения мерцания
+            timer.Enabled = true;
+
+        }
+
+        private void pictureBoxClock_Paint(object sender, PaintEventArgs e)
+        {
+            var graph = e.Graphics; 
+            graph.Clear(BackColor); 
+            graph.SmoothingMode = SmoothingMode.AntiAlias;
+            graph.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+
+            graph.TranslateTransform(pictureBoxClock.Width / 2, pictureBoxClock.Height / 2);
+           
+            DrawClockFace(graph);
+
+            e.Graphics.FillEllipse(Brushes.Black, -5, -5, 10, 10);
+
+            DrawClockHand(graph);
+        }
+
+        private void DrawClockFace(Graphics gr)
+        {
+            using(Pen tick_pen = new Pen(Color.Blue, 4))
+            {
+                gr.DrawEllipse(tick_pen,
+                    -pictureBoxClock.Width / 2 + 3, -pictureBoxClock.Height / 2 + 3,
+                    pictureBoxClock.Width - 5, pictureBoxClock.Height - 7);
+
+                float outer_x_factor = 0.45f * pictureBoxClock.Width;
+                float outer_y_factor = 0.45f * pictureBoxClock.Height;
+                float inner_x_factor = 0.425f * pictureBoxClock.Width;
+                float inner_y_factor = 0.425f * pictureBoxClock.Height;
+                float big_x_factor = 0.4f * pictureBoxClock.Width;
+                float big_y_factor = 0.4f * pictureBoxClock.Height;
+
+                tick_pen.StartCap = LineCap.Triangle;
+
+                for (int i = 0; i <= 360; i += 6)
+                {
+                    double angle = i * Math.PI / 180;
+                    float cos_angle = (float)Math.Cos(angle);
+                    float sin_angle = (float)Math.Sin(angle);
+                    PointF outer_pt = new PointF(
+                        outer_x_factor * cos_angle,
+                        outer_y_factor * sin_angle);
+                    if (i % 5 == 0)
+                    {
+                        PointF inner_pt = new PointF(
+                            big_x_factor * cos_angle,
+                            big_y_factor * sin_angle);
+                        gr.DrawLine(tick_pen, inner_pt, outer_pt);
+                    }
+                    else
+                    {
+                        PointF inner_pt = new PointF(
+                            inner_x_factor * cos_angle,
+                            inner_y_factor * sin_angle);
+                        gr.DrawLine(Pens.Blue, inner_pt, outer_pt);
+                    }
+                }
+
+            }
+        }
+
+        private void DrawClockHand(Graphics gr)
+        {
+            using(Pen tick_pen = new Pen(Color.Red, 4))
+            {
+                DateTime currentDate = DateTime.Now;
+                float hour = currentDate.Hour +
+                    currentDate.Minute / 60f +
+                    currentDate.Second / 3600f;
+
+                float minute = currentDate.Minute +
+                    currentDate.Second / 60f;
+
+                double x_factor = 0.2f * pictureBoxClock.Width;
+                double y_factor = 0.2f * pictureBoxClock.Height;
+
+                tick_pen.Color = Color.Red;
+
+                DrawHands(gr, x_factor, y_factor, hour / 12.0, tick_pen);
+
+                x_factor = 0.3f * pictureBoxClock.Width;
+                y_factor = 0.3f * pictureBoxClock.Height;
+
+                tick_pen.Width = 2;
+
+                DrawHands(gr, x_factor, y_factor, minute / 60.0, tick_pen);
+
+                x_factor = 0.4f * pictureBoxClock.Width;
+                y_factor = 0.4f * pictureBoxClock.Height;
+
+                DrawHands(gr, x_factor, y_factor, currentDate.Second / 60.0, tick_pen);
+            }
+        }
+
+        private void DrawHands(Graphics gr, double x_factor, double y_factor,
+            double value, Pen pen)
+        {
+            PointF center = new PointF(0, 0);
+
+            double hour_angle = -Math.PI / 2 + 2 * Math.PI * value;
+            PointF hour_pt = new PointF(
+                (float)(x_factor * Math.Cos(hour_angle)),
+                (float)(y_factor * Math.Sin(hour_angle)));
+
+            gr.DrawLine(pen, hour_pt, center);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            pictureBoxClock.Refresh();
+        }
+    }
+}
