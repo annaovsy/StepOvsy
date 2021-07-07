78815512fc77ca7ca238f2710c87edda5894b042
Anna Ovsyannikova <popkovaanna101@gmail.com>
2021-07-07 19:55:28 +0300

    d

---
+++ WPF/ResourcesAndStyle/Phone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourcesAndStyle
+{
+    class Phone
+    {
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public int Price  { get; set; }
+
+    }
+}
