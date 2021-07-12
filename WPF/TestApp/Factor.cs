d4463adad835b15709dda7c4049ed65a80c86a5a
Anna Ovsyannikova <popkovaanna101@gmail.com>
2021-07-12 19:17:18 +0300

    ;

---
+++ WPF/TestApp/Factor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace TestApp
+{
+    class FactorClass : INotifyPropertyChanged
+    {
+        private double _factor;
+
+        public double FactorTest
+        {
+            get { return _factor; }
+            set
+            {
+                _factor = value;
+                OnPropertyChanged(nameof(FactorTest));                
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
---
+++ WPF/TestApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FactorClass factor;
+        public MainWindow()
+        {
+            InitializeComponent();
+            factor = new FactorClass();
+            mainGrid.DataContext = factor;
+        }
+
+        private void checkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"{factor.FactorTest}");
+        }
+
+        private void increaseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            factor.FactorTest *= 2;
+        }
+    }
+}
